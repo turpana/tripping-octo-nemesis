46ec2fa25e1890b770a59f59babe54885ebc50c5
Turpana Molina <develop@turpana.com>
2012-08-20 22:12:15 -0600

    Typos

--- index.php
+++ index.php
@@ -10,21 +10,22 @@
       window.fbAsyncInit = function() {
         FB.init({
           appId : '<?php echo APPID; ?>',
-          channelUrl : '//app.turpana.com/channel.html',
+          channelUrl : 'http://app.turpana.com/channel.html',
           status : true,
           cookie : true,
           xfbml : true
         });
-        FB.api('/me', function(response) {
+        FB.getLoginStatus(function(response) {
           console.log(response);
-        });
+        }
       };
       // Load the SDK Asynchronously
       (function (d) {
         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName ('script')[0];
         if (d.getElementById(id)) {return;}
-          js = d.createElement('script'); js.id = id; js.async = true;
-        js.src = "//connect.facebook.net/en+us/all.js";
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
       }(document));
     </script>
 
